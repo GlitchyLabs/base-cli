144717966bf04498d404290b34ab28446da7022d
Dennis Bartlett <bartlett.dc.1@gmail.com>
2020-10-29 14:34:59 -0400

    Fixed echo module bug.

--- src/modules/echo/index.ts
+++ src/modules/echo/index.ts
@@ -8,5 +8,5 @@ program.parse(process.argv);
 // The first argument is normally the command, we setup not to send it, and then place an unused variable herebundleRenderer.renderToStream
 // This is due to the way commander handles this without a .command() call.  Will try to fix this later.
 export default function echo(_, text) {
-    logger.info(text);
+    logger.info(text.join(' '));
 }
